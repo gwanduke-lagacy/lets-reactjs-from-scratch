38cc344bc1028267e5da8bf6230ad9cdbfdffe07
letsget23 <letsget23@gmail.com>
2019-04-23 06:50:40 +0900

    Add React, ReactDOM

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Let's ReactJS From Scratch</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
