c27d510ca4c41edfbbda669de27d7773016df88c
letsget23 <letsget23@gmail.com>
2019-04-23 06:54:28 +0900

    Add HMR🔥

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Let's ReactJS From Scratch</h1>
+        <ul>
+          <li>🔥 Hot Module Reloading...</li>
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
